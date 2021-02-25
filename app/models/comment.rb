a4bcac8df5ef2e69cec8b26efdd5b9d03b7e9ddb
franklin <franklinbenjamin032@gmail.com>
2021-02-25 13:53:02 -0400

    add validation to comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :post 
   belongs_to :user
+
+  validates :commenter, presence: true, length: { minimum: 8}
+  validates :content, presence: true, length: { maximum: 15}
 end
