82acc809de6ead09da7609a9ccb76d4c7a523fd0
franklin <franklinbenjamin032@gmail.com>
2021-02-25 17:36:36 -0400

    add validation to post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comments
+
+  validates :commenter, presence: true, length: { minimum: 8}
+  validates :content, presence: true, length: { maximum: 200}
 end
