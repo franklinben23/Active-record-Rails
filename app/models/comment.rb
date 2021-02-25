ce7967f2f2cb5e848b5ae85e0cc325c229c4d26c
franklin <franklinbenjamin032@gmail.com>
2021-02-25 17:47:22 -0400

    linter fix

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,6 @@ class Comment < ApplicationRecord
   belongs_to :post 
   belongs_to :user
 
-  validates :commenter, presence: true, length: { minimum: 8}
-  validates :content, presence: true, length: { maximum: 15}
+  validates :commenter, presence: true, length: { minimum: 8 }
+  validates :content, presence: true, length: { maximum: 15 }
 end
