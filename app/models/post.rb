7cd631ba03f4f65baa9fe6817ebb51e493e1f98b
franklin <franklinbenjamin032@gmail.com>
2021-02-25 18:22:12 -0400

    validation correction

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments
 
-  validates :commenter, presence: true, length: { minimum: 8 }
+  validates :title, presence: true, length: { maximum: 20 }
   validates :content, presence: true, length: { maximum: 200 }
 end
